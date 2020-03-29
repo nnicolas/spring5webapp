6f983275f048a7830189afb01c24e6d1eb9bc47d
Najy Nicolas <najy.nicolas@gmail.com>
2020-03-29 14:12:07 -0400

    Testing git

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5webappApplication {
 
+	/*
+	 * Najy test
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
